b8fca8d7ded589831689a4dfadf76a55f485c43f
fooofei <aihujianfei@gmail.com>
2017-12-25 23:32:55 +0800

    add stringstream

---
+++ ostringstream_sprintf.cpp
@@ -0,0 +1,25 @@
+
+#include <sstream>
+#include <stdio.h>
+int main() 
+{
+  std::ostringstream oss;
+
+  oss << "123"
+      << " " 
+      << 4 
+      << 5;
+
+  printf("%s\n", oss.str().c_str());
+  
+  // reset the stream
+  oss.str(std::string());
+  oss.clear();
+
+  oss<<1.345f;
+
+  printf("%s\n", oss.str().c_str());
+
+  printf("hello\n");
+  return 0;
+}
