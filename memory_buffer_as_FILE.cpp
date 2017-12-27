2361ced4412d1e854cb4eed179e0969f71cf2bcf
fooofei <aihujianfei@gmail.com>
2017-12-27 22:33:06 +0800

    format

--- memory_buffer_as_FILE.cpp
+++ memory_buffer_as_FILE.cpp
@@ -5,34 +5,26 @@
 
 int main()
 {
-
     std::string buffer;
     buffer.resize(2048);
     FILE * f = 0;
 
     f = fmemopen(&buffer[0], buffer.size(), "wb");
 
-
     if (f)
     {
 
         fwrite("nice", 4, 1, f);
         fprintf(f, "hello");
-
         fprintf(f, "world");
-
+       
         fflush(f);
-
         printf("%s\n", buffer.c_str()); // nice hello world
-
 
         fclose(f);
     }
 
     printf("end\n");
 
-
-
-   
     return 0;
 }
