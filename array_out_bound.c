fe728697dc454557eca84852e024fdf552788771
fooofei <aihujianfei@qq.com>
2020-06-25 15:50:36 +0800

    format

--- array_out_bound.c
+++ array_out_bound.c
@@ -1,24 +1,22 @@
-
 #include <stdio.h>
 #include <stdint.h>
 
 
 int main()
 {
+    uint32_t i = 0;
+    uint8_t b[1028];
 
-  uint32_t i = 0;
-  uint8_t b[1028];
 
+    /*
+      linux
+        debug - core dumped
+        release - no-core
+      Windows x86
+        debug - no crash
+        release - no crash
+    */
+    b[i] = b[i - 1];
 
-  /*
-    linux 
-      debug - core dumped
-      release - no-core
-    Windows x86
-      debug - no crash
-      release - no crash
-  */
-  b[i] = b[i - 1];
-
-  return 0;
+    return 0;
 }
